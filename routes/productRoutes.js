532dc8c5daa51b54cb43169fa24089a24d9d9bc3
dhanasekar1418 <dhanasekar.v2022eee@sece.ac.in>
2024-08-13 23:14:47 +0530

    feat4

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,12 +1,12 @@
-const Productcontroller = require('../controllers/productController')
-const express = require('express');
+const Productcontroller = require("../controllers/productController");
+const express = require("express");
 const router = express.Router();
-const auth = require('../controllers/middlewares/auth');
+const auth = require("../controllers/middlewares/auth");
 
-router.get("/",auth, Productcontroller.getProducts);
-router.post("/",Productcontroller.createProduct);
+router.get("/", Productcontroller.getProducts);
+router.post("/", Productcontroller.createProduct);
 router.delete("/:id", Productcontroller.deleteProduct);
-router.put("/:id",Productcontroller.updateProduct);
+router.put("/:id", Productcontroller.updateProduct);
 router.patch("/:id", Productcontroller.patchProduct);
 
-module.exports = router
+module.exports = router;
